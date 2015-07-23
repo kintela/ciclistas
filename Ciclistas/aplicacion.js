5c98faa231eb065f95a257e346510feee91e5e1b
kintela <ROBERTO.QUINTELA@GMAIL.COM>
2015-07-24 01:11:18 +0200

    Ejemplos con Directivas

---
+++ Ciclistas/aplicacion.js
@@ -0,0 +1,10 @@
+﻿angular.module('miApp', [])
+       .directive('miDirectiva', [function () {
+           return {
+               restrict: 'E',
+               replace: true,
+               link: function (scope, iElement, iAttrs) {
+               },
+               template: '<p>Este es el párrafo de la directiva</p>'
+           };
+}]);
