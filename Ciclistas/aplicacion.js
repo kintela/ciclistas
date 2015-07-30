9e611bea064b2fdaf490fca5a1d0dbac24d2cc9f
kintela <roberto.quintela@gmail.com>
2015-07-30 17:39:03 +0200

    formularios

--- Ciclistas/aplicacion.js
+++ Ciclistas/aplicacion.js
@@ -1,10 +1,65 @@
 ﻿angular.module('miApp', [])
-       .directive('miDirectiva', [function () {
+       .directive('cajaBusqueda', function () {
            return {
                restrict: 'E',
-               replace: true,
-               link: function (scope, iElement, iAttrs) {
+               transclude: false,
+               scope: {
+                   textoABuscar: '=',
+                   resultados:'='
                },
-               template: '<p>Este es el párrafo de la directiva</p>'
+               controller: function ($scope) {
+                   $scope.textoLocal = '';
+                   $scope.borrarBusqueda = function () {
+                       $scope.textABuscar = "";
+                       $scope.textoLocal = "";
+                   };
+                   $scope.Buscar = function () {
+                       $scope.textABuscar = $scope.textoLocal;
+                   };
+               },
+               replace: true,
+               template:
+                   '<form>'+
+                        '<div>' +
+                            '<input ng-model="textoLocal" type="text" />' +
+                        '</div>' +
+                        '<div>' +
+                            '<button ng-click="borrarBusqueda()">Borrar</button>' +
+                            '<button ng-click="Buscar()">Buscar</button>' +
+                        '</div>' +
+                    '</form>'
            };
-}]);
+});
+angular.module("miApp", []).
+        directive('resultadosBusqueda', function () {
+            return {
+                restrict: 'E',
+                transclude: true,
+                scope: {
+                    resultados: '=',
+                    textoABuscar: '='
+                },
+                replace: true,
+                template:
+                '<div>' +
+                    '<h4 ng-show="resultados">Encontrados {{resultados.length}} resultados de la búsqueda :</h4>' +
+                    '<ul>' +
+                        '<li ng-repeat="resultado in resultados">' +
+                            '{{resultado}}' +
+                        '</li>' +
+                    '</ul>' +
+                '</div>'
+            };
+        });
+angular.module("miApp", []).controller("ControladorBusquedas", function ($scope, $timeout) {
+    $scope.$watch("corredoresBuscados", function (corredoresBuscados) {
+        if (corredoresBuscados) {
+            $scope.corredoresEncontrados = [];
+            $timeout(function () { // simular retardo de búsqueda de 1 seg. siempre con el mismo resultado
+                $scope.corredoresEncontrados = ['Froome', 'Contador', 'Nibali', 'Quintana', 'Cancellara'];
+            }, 1000);
+        } else {
+            $scope.corredoresEncontrados = [];
+        }
+    });
+});
--- Ciclistas/app.js
+++ Ciclistas/app.js
@@ -1,7 +1,67 @@
-﻿function HolaMundo() {
-    return "Hola Mundo";
-}
+﻿angular.module('miApp', [])
+       .directive('cajaBusqueda', function () {
+           return {
+               restrict: 'E',
+               transclude: false,
+               scope: {
+                   textoABuscar: '=',
+                   resultados: '='
+               },
+               controller: function ($scope) {
+                   $scope.textoLocal = '';
+                   $scope.borrarBusqueda = function () {
+                       $scope.textABuscar = "";
+                       $scope.textoLocal = "";
+                   };
+                   $scope.Buscar = function () {
+                       $scope.textABuscar = $scope.textoLocal;
+                   };
+               },
+               replace: true,
+               template:
+                   '<form>' +
+                        '<div>' +
+                            '<input ng-model="textoLocal" type="text" />' +
+                        '</div>' +
+                        '<div>' +
+                            '<button ng-click="borrarBusqueda()">Borrar</button>' +
+                            '<button ng-click="Buscar()">Buscar</button>' +
+                        '</div>' +
+                    '</form>'
+           };
+       });
 
-function Sumar(x, y) {
-    return x + y;
-}
+angular.module("miApp", []).
+        directive('resultadosBusqueda', function () {
+            return {
+                restrict: 'E',
+                transclude: true,
+                scope: {
+                    resultados: '=',
+                    textoABuscar: '='
+                },
+                replace: true,
+                template:
+                '<div>' +
+                    '<h4 ng-show="resultados">Encontrados {{resultados.length}} resultados de la búsqueda :</h4>' +
+                    '<ul>' +
+                        '<li ng-repeat="resultado in resultados">' +
+                            '{{resultado}}' +
+                        '</li>' +
+                    '</ul>' +
+                '</div>'
+            };
+        });
+
+angular.module("miApp", []).controller("ControladorBusquedas", function ($scope, $timeout) {
+    $scope.$watch("corredoresBuscados", function (corredoresBuscados) {
+        if (corredoresBuscados) {
+            $scope.corredoresEncontrados = [];
+            $timeout(function () { // simular retardo de búsqueda de 1 seg. siempre con el mismo resultado
+                $scope.corredoresEncontrados = ['Froome', 'Contador', 'Nibali', 'Quintana', 'Cancellara'];
+            }, 1000);
+        } else {
+            $scope.corredoresEncontrados = [];
+        }
+    });
+});
