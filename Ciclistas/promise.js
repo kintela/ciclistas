6ddb43d192241c4d91e74b30fdca835ed50ba1cb
kintela <ROBERTO.QUINTELA@GMAIL.COM>
2015-08-12 18:06:00 +0200

    promises

---
+++ Ciclistas/promise.js
@@ -0,0 +1,29 @@
+﻿var app = angular.module("app", ["firebase"]);
+
+app.controller("controlador2", function ($scope, $q, $firebase) {
+    var objDiferido = $q.defer();
+    var URL = "https://flickering-fire-9197.firebaseio.com/";
+
+    objDiferido.promise.then(function () {
+        var refDatos = new Firebase(URL);
+        return $firebase(refDatos);
+    }).then(function (datos) {
+        $scope.ciclistas = datos;
+    });
+    // Aquí no pasamos ningún argumento, pero podríamos pasar la URL
+    objDiferido.resolve(); // Pone en marcha las promises definidas antes
+});
+
+app.controller("controlador",['$scope','$filter','$firebase','$q', function ($scope,$filter, $firebase,$q) {
+    var objDiferido = $q.defer();
+    var URL = "https://flickering-fire-9197.firebaseio.com/";
+
+    objDiferido.promise.then(function () {
+        var refDatos = new Firebase(URL);
+        return $firebase(refDatos);
+    }).then(function (datos) {
+        $scope.ciclistas = datos;
+    });
+    // Aquí no pasamos ningún argumento, pero podríamos pasar la URL
+    objDiferido.resolve(); // Pone en marcha las promises definidas antes
+}]);
